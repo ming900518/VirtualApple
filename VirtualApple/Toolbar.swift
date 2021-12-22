e9f595954e728305bdff79ffe46bcdd23f9fd61a
Ming Chang <ming@mingchang137.su>
2021-12-22 13:38:19 +0800

    Add customizations.

--- VirtualApple/Toolbar.swift
+++ VirtualApple/Toolbar.swift
@@ -8,41 +8,44 @@
 import AppKit
 
 enum ToolbarIdentifiers: String, CaseIterable {
-	case settings
-	case toggleState
-	
-	var toolbarItemIdentifier: NSToolbarItem.Identifier {
-		.init(rawValue)
-	}
-	
-	init?(_ rawValue: NSToolbarItem.Identifier) {
-		self.init(rawValue: rawValue.rawValue)
-	}
+    case settings
+    case toggleState
+    
+    var toolbarItemIdentifier: NSToolbarItem.Identifier {
+        .init(rawValue)
+    }
+    
+    init?(_ rawValue: NSToolbarItem.Identifier) {
+        self.init(rawValue: rawValue.rawValue)
+    }
 }
 
 extension WindowController: NSToolbarDelegate {
-	func toolbarAllowedItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
-		return ToolbarIdentifiers.allCases.map(\.toolbarItemIdentifier)
-	}
-	
-	func toolbarDefaultItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
-		return toolbarAllowedItemIdentifiers(toolbar)
-	}
-	
-	func toolbar(_ toolbar: NSToolbar, itemForItemIdentifier itemIdentifier: NSToolbarItem.Identifier, willBeInsertedIntoToolbar flag: Bool) -> NSToolbarItem? {
-		let toolbarItem = NSToolbarItem(itemIdentifier: itemIdentifier)
-		switch ToolbarIdentifiers(itemIdentifier)! {
-		case .settings:
-			toolbarItem.image = NSImage(systemSymbolName: "gearshape", accessibilityDescription: "Settings")
-			toolbarItem.label = "Settings"
-			toolbarItem.target = viewController
-			toolbarItem.action = #selector(viewController.openSettings(_:))
-		case .toggleState:
-			toolbarItem.image = NSImage(systemSymbolName: "play", accessibilityDescription: "Run")
-			toolbarItem.label = "Run"
-			toolbarItem.target = viewController
-			toolbarItem.action = #selector(ViewController.toggleState(_:))
-		}
-		return toolbarItem
-	}
+    func toolbarAllowedItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
+        return ToolbarIdentifiers.allCases.map(\.toolbarItemIdentifier)
+    }
+    
+    func toolbarDefaultItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
+        return toolbarAllowedItemIdentifiers(toolbar)
+    }
+    
+    func toolbar(_ toolbar: NSToolbar, itemForItemIdentifier itemIdentifier: NSToolbarItem.Identifier, willBeInsertedIntoToolbar flag: Bool) -> NSToolbarItem? {
+        let toolbarItem = NSToolbarItem(itemIdentifier: itemIdentifier)
+        switch ToolbarIdentifiers(itemIdentifier)! {
+        case .settings:
+            toolbarItem.image = NSImage(systemSymbolName: "gearshape", accessibilityDescription: "Settings")
+            toolbarItem.label = "Settings"
+            toolbarItem.target = viewController
+            toolbarItem.action = #selector(viewController.openSettings(_:))
+        case .toggleState:
+            toolbarItem.image = NSImage(systemSymbolName: "play", accessibilityDescription: "Run")
+            toolbarItem.label = "Run"
+            toolbarItem.target = viewController
+            toolbarItem.action = #selector(ViewController.toggleState(_:))
+        }
+        return toolbarItem
+    }
+    func window(_ window: NSWindow, willUseFullScreenPresentationOptions proposedOptions: NSApplication.PresentationOptions = []) -> NSApplication.PresentationOptions {
+        return [.hideDock, .autoHideMenuBar, .autoHideToolbar, .fullScreen]
+    }
 }
